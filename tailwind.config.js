58614ed602332c9a1e0b0b2147af6c85d071fa62
mostafa niakan <mostafaniakan96@gmail.com>
2022-05-26 13:47:59 +0430

    personal navbar

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  content: ["*/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        main: "#E8EDF0",
+        btnHeader: "linear-gradient(145deg, #e2e8ec, #ffffff)",
+        btnColor: "#ff014f",
+        GradientBtn: "#d11414",
+        shadowColor: "#e2e8ec",
+        navbarLi: "#878e99",
+      },
+      boxShadow: {
+        "4x1": "5px 5px 15px #D1D9E6, -5px -5px 15px #ffffff;",
+      },
+      spacing: {
+        350: "350px",
+      },
+    },
+  },
+  plugins: [],
+};
