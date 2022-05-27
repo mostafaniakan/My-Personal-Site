344b6889cf200ac1b0fda2f88b6b2f9d505f246f
mostafa niakan <mostafaniakan96@gmail.com>
2022-05-27 18:08:57 +0430

    resume btn

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     extend: {
       colors: {
         main: "#E8EDF0",
-        btnHeader: "linear-gradient(145deg, #e2e8ec, #ffffff)",
+
         btnColor: "#ff014f",
         GradientBtn: "#d11414",
         shadowColor: "#e2e8ec",
@@ -13,6 +13,7 @@ module.exports = {
       boxShadow: {
         "4x1": "5px 5px 15px #D1D9E6, -5px -5px 15px #ffffff;",
       },
+
       spacing: {
         350: "350px",
       },
